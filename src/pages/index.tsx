ccb5f2f7d410e5133898052c1c9d05b8475edfce
marciovz <marciovz@yahoo.com.br>
2023-06-26 15:03:34 -0300

    configução da biblioteca stitches css-in-js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,25 @@
+import { styled } from "../styles"
+
+const Container = styled('div', {
+  width: 900,
+  margin: '0 auto',
+  background: '$background',
+  height: 600
+})
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  borderRadius:4,
+  border: 0,
+  padding: '4px 8px',
+})
+
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <Container>
+      <h1>Hello World</h1>
+      <Button>Enviar</Button>
+    </Container>
   )
 }
---
+++ src/styles/index.ts
@@ -0,0 +1,19 @@
+import { createStitches } from '@stitches/react';
+
+export const {
+  config,
+  styled,
+  css,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme
+} = createStitches({
+  theme: {
+    colors: {
+      rocketseat: '#8257e6',
+      background: '#c8c8c8',
+    }
+  }
+})
