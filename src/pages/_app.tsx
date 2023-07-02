07643277597d62e4d83092e9875edbe3235e4084
marciovz <marciovz@yahoo.com.br>
2023-07-02 16:12:29 -0300

    Adicionado o link de redirecionamento na logo do Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import type { AppProps } from 'next/app'
-import Image from 'next/image';
+import Image from 'next/image'
+import Link from 'next/link'
 
 import logoImg from '../assets/logo.svg'
 
-import { globalStyles } from '../styles/global';
-import { Container, Header } from '../styles/pages/app';
+import { globalStyles } from '../styles/global'
+import { Container, Header } from '../styles/pages/app'
 
 globalStyles();
 
@@ -12,7 +13,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" />
+        <Link href={'/'}>
+          <Image src={logoImg} alt="" />
+        </Link>
       </Header>
 
       <Component {...pageProps} />
